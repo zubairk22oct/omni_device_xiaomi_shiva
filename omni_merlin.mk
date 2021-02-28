6c4259a13f205523726092455612ab6656c4839e
JamieHoSzeYui <henloboii@protonmail.ch>
2021-02-28 10:47:56 +0000

    merlin: pb -> omni

--- omni_merlin.mk
+++ omni_merlin.mk
@@ -16,30 +16,20 @@
 # limitations under the License.
 #
 
-# PBRP-10.0 EDITS - START
-
 PRODUCT_RELEASE_NAME := merlin
-# PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 DEVICE_PATH := device/xiaomi/merlin
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/pb/config/common.mk)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
-# Inherit PBRP stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
-
-# PBRP-10.0 EDITS - END
-
 # Specify phone tech before including full_phone
-# $(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit some common Omni stuff.
-# $(call inherit-product, vendor/omni/config/common.mk)
-# $(call inherit-product, build/target/product/embedded.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit Telephony packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -54,6 +44,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 PRODUCT_DEVICE := merlin
 PRODUCT_NAME := omni_merlin
 PRODUCT_BRAND := xiaomi
-PRODUCT_MODEL := Xiaomi Redmi Note 9
+PRODUCT_MODEL := Redmi Note 9
 PRODUCT_MANUFACTURER := xiaomi
-PRODUCT_RELEASE_NAME := Xiaomi Redmi Note 9
+PRODUCT_RELEASE_NAME := Redmi Note 9
